e776a7ee5d1c67e509e6a7a5bffb836f24dd797d
murattick <murattick@gmail.com>
2017-07-19 15:45:43 +0300

    test

---
+++ BankLibrary/DemandAccount.cs
@@ -0,0 +1,16 @@
+﻿namespace BankLibrary
+{
+    //счёт до востробования
+    public class DemandAccount : Account
+    {
+        public DemandAccount(decimal sum, int percentage) : base ( sum, percentage)
+        {
+        }
+
+        //переопределяе метод добавления счёта
+        protected internal override void Open()
+        {
+            base.OnOpened(new AccountEventArgs("Открыт новый счет до востребования! Id счета: " + this._id, this._sum));
+        }
+    }
+}
---
+++ BankLibrary/IAccount.cs
@@ -0,0 +1,11 @@
+﻿namespace BankLibrary
+{
+    public interface IAccount
+    {
+        //положить деньги на счёт
+        void Put(decimal sum);
+
+        //взять деньги с счёта
+        decimal Withdraw(decimal sum);
+    }
+}
